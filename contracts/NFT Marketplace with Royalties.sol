44261dbf36143f5f54bf0591bbda5405d3c20043
Alamgir7897 <alamgiransari7897@gmail.com>
2025-06-18 23:40:41 +0530

    Update NFT Marketplace with Royalties.sol

--- contracts/NFT Marketplace with Royalties.sol
+++ contracts/NFT Marketplace with Royalties.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.s]]ol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
